13f3ef9955c5bac0697c91e86aaa88d99dadf715
amira <amira.world@hotmail.fr>
2020-07-27 16:03:24 +0200

    findpartner.js modified

--- scripts/findPartner.js
+++ scripts/findPartner.js
@@ -15,16 +15,6 @@ findPartner=  () =>{
 
     partnerList.forEach( e => {
         if ( e.time == timeSelect.value && e.workout == workoutSelect.value ){
-
-            // document.querySelector('.noPartner').style.visibility = "hidden";
-            // document.getElementById('displayPartner').innerHTML +=`
-            // <div class="card grid-item" style="width: 18rem;">
-            //     <i class="fas fa-user fa-7x"></i>
-            //     <div class="card-body">
-            //         <h5 class="card-title">${e.name}</h5>
-            //         <a href="#" class="btn btn-success" id="inviteBtn">Invite</a>
-            //     </div>
-            // </div>`
          document.querySelector('.noPartner').style.visibility = "hidden";
             document.getElementById('displayPartner').innerHTML +=`
             <div class="card grid-item" style="width: 18rem;">
@@ -37,12 +27,13 @@ findPartner=  () =>{
 
         }else{
             console.log('no match found');
-            //  document.querySelector('.noPartner').innerHTML = `
-            // <div class="container noPartner">
-            //     <i class="fas fa-frown-open fa-7x"></i></br>
-            //     <p>Sorry no one is available </p>
-            // </div>
-            // `
+
+             document.querySelector('.noPartner').innerHTML = `
+            <div class="container noPartner">
+                <i class="fas fa-frown-open fa-7x"></i></br>
+                <p>Sorry no one is available </p>
+            </div>
+            `
 
         }
     });
@@ -63,9 +54,7 @@ document.getElementById('findBtn').addEventListener('click', (e)=>{
             window.open("confirmInvitation.html")
        });
     })
-    // document.querySelector('.inviteBtn').addEventListener('click', ()=>{
-    //      window.open("confirmInvitation.html")
-    // });
+    
 });
 
 
