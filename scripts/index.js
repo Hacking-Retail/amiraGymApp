c5c7fccc5bcbeb0c23f26badb6a9cb2a37ba1d55
amira <amira.world@hotmail.fr>
2020-07-27 16:26:55 +0200

    index html modif

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-var quotes = ['All progress takes place outside the comfort zone- Michael John Bobak',
-  'The clock is ticking. Are you becoming the person you want to be? -  Greg Plitt',
-  'Action is the foundational key to all success - Pablo Picasso',
+var quotes = ['All progress takes place outside your comfort zone',
+  'The clock is ticking. Are you becoming the person you want to be? ',
+  'Action is the foundational key to all success ',
   ]
 function newQuote() {
 var randomNumber = Math.floor(Math.random() * (quotes.length));
